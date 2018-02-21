f2376050ea438114b4c38fea7549d75a34adf3fd
almase <alberto.puche@gmail.com>
2018-02-21 15:42:57 +0100

    Hasta funciones

---
+++ lib/m1.py
@@ -0,0 +1,22 @@
+import sys 
+
+
+def saludar(saludo):
+	print saludo
+
+def iniciales(nombre,ape1,ape2):
+	iniciales=nombre[0]+'.'+ape1[0]+'.'+ape2[0]+'.'
+	return "Tus iniciales son:"+iniciales.upper()
+
+
+def iniciales1(nombre,ape1,*apellidos):
+	iniciales=nombre[0]+'.'+ape1[0]
+	for ape in apellidos:
+		iniciales=iniciales+'.'+ape[0]
+	return iniciales.upper()
+
+
+
+
+
+
